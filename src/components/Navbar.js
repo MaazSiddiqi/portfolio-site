b209e9bbafb735e14253aeb6232bd30a02ca2f93
Maaz Ali <maaz@Maazs-MacBook-Air.local>
2022-01-04 21:13:20 -0500

    Fixed hero spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar({ mainText, menuItems, special, shrinkAt = 'sm' }
     <nav className="flex bg-gradient-to-b from-gray-200/75 py-6 text-center text-gray-600 fixed w-full z-50">
       <div className="flex justify-between w-full mx-10 md:mx-24 xl:mx-40">
         {/* Nav Main */}
-        <a href="#" className="text-2xl font-mono border-b-2 border-indigo-600">
+        <a href="#" className=" p-1 text-2xl font-mono border-b-2 border-indigo-600 hover:scale-105 hover:shadow-lg hover:rounded-xl active:scale-95 active:shadow-inner active:bg-gray-100 transition-all duration-200">
           {mainText ? mainText : "Nav Main."}
         </a>
 
@@ -29,14 +29,14 @@ export default function Navbar({ mainText, menuItems, special, shrinkAt = 'sm' }
           )}
         </span>
 
-        {/* Nav Condensed Sidemenu */}
+        {/* Nav Condensed Sidemenu Button */}
         <button
           onClick={(e) => {
             setShowSideMenu(!showSideMenu);
           }}
           className={`${shrinkAt}:hidden ${
             showSideMenu ? "hidden" : ""
-          } rounded-xl bg-indigo-600 text-white hover:scale-105 hover:shadow-md hover:shadow-violet-600/50 hover:bg-white hover:text-gray-600 transition-all duration-300 p-1 active:shadow-inner active:scale-95 `}
+          } rounded-xl bg-indigo-600 text-white hover:scale-105 hover:shadow-md hover:shadow-violet-600/50 hover:bg-white hover:text-gray-600 transition-all duration-150 p-1 active:shadow-inner active:scale-95 `}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -54,10 +54,11 @@ export default function Navbar({ mainText, menuItems, special, shrinkAt = 'sm' }
           </svg>
         </button>
         
+        {/* Nav Condensed SideMenu Panel */}
         <div
           className={`${
             showSideMenu ? "" : "hidden"
-          } ${shrinkAt}:hidden bg-gradient-to-l from-white fixed right-0 top-0 h-full w-full font-light`}
+          } ${shrinkAt}:hidden bg-gradient-to-l from-white absolute top-0 right-0 h-full w-full font-light`}
         >
           <ul className="w-full text-right">
             <button
@@ -68,7 +69,7 @@ export default function Navbar({ mainText, menuItems, special, shrinkAt = 'sm' }
               <li className="pt-8 pb-2 px-6 group w-full hover:cursor-pointer">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 group-hover:scale-125 group-hover:rotate-180 transition-all duration-300"
+                  className="h-6 w-6 group-hover:scale-125 group-hover:rotate-180 transition-all duration-200"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
