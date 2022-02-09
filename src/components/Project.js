44f6b024af7b53af0589a9abdb59812f0c31ee65
Maaz Ali <maaz@Maazs-MacBook-Air.local>
2022-02-08 19:00:15 -0500

    missed couple changes from last commit

---
+++ src/components/Project.js
@@ -0,0 +1,18 @@
+import Card from "./Card";
+export default function Project({ title, stack, img, detail }) {
+  return (
+    <div className="flex flex-col items-center">
+      <Card grow img={img}>
+        <div className="space-y-4 bg-[./images/RandomCode.jpeg]">
+          <div>
+            <h1 className="text-xl font-semibold text-gray-700">{title}</h1>
+            <h2 className="italic font-medium">
+              <span className="text-indigo-500">Tech Stack:</span> {stack}
+            </h2>
+          </div>
+          <p className="font-light">{detail}</p>
+        </div>
+      </Card>
+    </div>
+  );
+}
