050576a1cf6305c307f406fe9ce0fe01b4dab361
Maaz Ali <maazali22@gmail.com>
2023-04-01 03:43:30 -0400

    fixed experiences screen

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,11 +1,8 @@
 import React from "react"
-import Project from "./Project"
-import Card from "./Card"
-import { useState } from "react"
-import Experience from "./Experience"
+import Experience from "./Cards/Experience"
 
 // Typically would be retrived through db
-const experiences = [
+export const experiences = [
   {
     title: "Software Development Intern",
     company: "Honest Empathy",
@@ -21,45 +18,24 @@ const experiences = [
     stack: "MongoDB, ExpressJS, ReactJS, NodeJS, Socket.io",
     detail:
       "I developed a real-time chat feature using WebSockets and Socket.io, optimized database design, and reduced API request times by 50%. I also created a model for implementing significant database schema changes while preserving existing data.",
-    date: "Sept. 2022 - Present",
+    date: "June 2022 - Oct. 2022",
     link: "",
   },
 ]
 
-export default function Projects() {
+export default function Experiences() {
   return (
-    <section className="min-h-screen bg-white ">
-      <div
-        id="projects"
-        className="flex flex-col items-center p-16 space-y-12 text-gray-500"
-      >
-        <div className="flex flex-col text-center">
-          <h1 className="text-3xl text-center font-semibold text-gray-700 mb-6">
-            My Experience.
-          </h1>
-          <p>
-            <span className="font-medium">My Tech Stack: </span>
-            <span className="sm:gradient-text text-indigo-500 italic font-medium">
-              Java, TypeScript, Python, C#, Unity, HTML, CSS, JavaScript,
-              ReactJS, NodeJS, ExpressJS
-            </span>
-          </p>
-          <p>Here is an overview of my work experience so far!</p>
-        </div>
-
-        <div className="space-y-6">
-          {experiences.map(({ title, company, stack, detail, date, link }) => (
-            <Experience
-              title={company}
-              subtitle={title}
-              stack={stack}
-              detail={detail}
-              date={date}
-              link={link}
-            />
-          ))}
-        </div>
-      </div>
-    </section>
+    <>
+      {experiences.map(({ title, company, stack, detail, date, link }) => (
+        <Experience
+          title={company}
+          subtitle={title}
+          stack={stack}
+          detail={detail}
+          date={date}
+          link={link}
+        />
+      ))}
+    </>
   )
 }
