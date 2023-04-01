222ae090275618791da670b9f7a45810f0c61c55
Maaz Ali <maazali22@gmail.com>
2023-04-01 01:51:04 -0400

    reforamtted about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
       <div className="space-y-8">
         <div className="max-w-5xl space-y-8 text-slate-50">
           <h1 className=" font-bold text-3xl text-slate-100">About Me.</h1>
-          <div className="text-lg space-y-6 font-medium">
+          <div className="text-lg space-y-6 font-light">
             <p>
               Hey, my name is Maaz Siddiqi! I'm a passionate self-taught
               developer who just started studying at Western University in Fall
@@ -25,36 +25,14 @@ export default function About() {
               unforgettable! I've met all kinds of people, picking up a variety
               of skills in the process.
             </p>
+            <p className="font-light">
+              <span className="text-white mb-2 font-semibold text-transparent">
+                Interests:
+              </span>{" "}
+              Machine learning and AI, Augmented Reality, Hackathons, Fitness,
+              Badminton, Basketball, Cooking
+            </p>
           </div>
-        </div>
-
-        {/* Cards */}
-        <div className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-5 justify-between">
-          <Card title="Employment" grow>
-            <div className="text-gray-500 mt-2">
-              <h2 className="mb-2 font-semibold bg-clip-text bg-gradient-to-r from-indigo-600 via-purple-600 to-fuchsia-500 text-transparent">
-                Cashier / Self-Checkout Attendant
-              </h2>
-              <p className="font-light">Walmart Inc.</p>
-              <p className="font-light">Since July 2021</p>
-            </div>
-          </Card>
-          <Card title="Hobbies and Interests" grow>
-            <ul className="text-gray-500 mt-2">
-              <li className="mb-2 font-semibold bg-clip-text bg-gradient-to-r from-indigo-600 via-purple-600 to-fuchsia-500 text-transparent">
-                Basketball
-              </li>
-              <li className="mb-2 font-semibold bg-clip-text bg-gradient-to-r from-indigo-600 via-purple-600 to-fuchsia-500 text-transparent">
-                Game Development
-              </li>
-              <li className="mb-2 font-semibold bg-clip-text bg-gradient-to-r from-indigo-600 via-purple-600 to-fuchsia-500 text-transparent">
-                Investing
-              </li>
-              <li className="mb-2 font-semibold bg-clip-text bg-gradient-to-r from-indigo-600 via-purple-600 to-fuchsia-500 text-transparent">
-                Physical Fitness
-              </li>
-            </ul>
-          </Card>
         </div>
       </div>
     </section>
