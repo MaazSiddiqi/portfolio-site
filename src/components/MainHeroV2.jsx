8395b64be9594072c6a8c9ec3fe725e2d33b104b
Maaz Ali <maazali22@gmail.com>
2023-04-18 13:32:35 -0400

    disabled float

--- src/components/MainHeroV2.jsx
+++ src/components/MainHeroV2.jsx
@@ -33,15 +33,15 @@ export default function MainHero() {
             position={[0, 5, 10]}
             intensity={0.7}
           />
-          <Float
+          {/* <Float
             speed={0.8} // Animation speed, defaults to 1
             rotationIntensity={0.2} // XYZ rotation intensity, defaults to 1
             floatIntensity={0.02} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
-          >
-            <mesh rotation={[0, -Math.PI / 24, 0]} position={[0, 0, -5]}>
-              <MainText />
-            </mesh>
-          </Float>
+          > */}
+          <mesh rotation={[0, -Math.PI / 24, 0]} position={[0, 0, -5]}>
+            <MainText />
+          </mesh>
+          {/* </Float> */}
         </Center>
         <Backdrop />
         <EffectComposer>
