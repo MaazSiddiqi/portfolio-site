1b4d75e4bcb6d10e5ece07d65534da98efd6726d
Maaz Ali <maazali22@gmail.com>
2023-04-18 02:02:47 -0400

    changed styling

--- src/components/MainHeroV2.jsx
+++ src/components/MainHeroV2.jsx
@@ -1,9 +1,9 @@
-import { Center, OrbitControls, Text3D } from "@react-three/drei"
+import { Center, OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import React from "react"
 import Deadzone from "./three/Deadzone"
-import Backdrop from "./three/PointsSpheres"
 import MainText from "./three/MainText"
+import Backdrop from "./three/PointsSpheres"
 
 export default function MainHero() {
   return (
--- src/components/three/Deadzone.jsx
+++ src/components/three/Deadzone.jsx
@@ -5,7 +5,7 @@ export default function Deadzone() {
     <div className="w-screen h-1/5 absolute bottom-0 grid place-items-center group">
       <div className="w-full h-full opacity-0 group-hover:opacity-100 bg-gradient-to-t from-black/25 via-slate-500/25 to-transparent absolute top-0 transition-opacity duration-150"></div>
       <a
-        className="pt-10 text-blue-800 hover:drop-shadow-lg group-hover:text-white transition-colors duration-250"
+        className="pt-10 text-blue-800 hover:drop-shadow-lg group-hover:text-slate-300 transition-colors duration-250"
         href="#about"
       >
         <svg
