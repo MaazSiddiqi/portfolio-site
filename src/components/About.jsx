ddfa38214033dd5cc5372387dfb146dea72dcd59
Maaz Ali <maazali22@gmail.com>
2023-04-18 16:54:05 -0400

    about me margins

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,17 @@ export default function About() {
   return (
     <section
       id="about"
-      className="flex flex-col justify-center items-center space-y-8 md:space-x-14 min-h-[25rem] bg-gradient-to-br from-indigo-600 via-purple-600 to-fuchsia-500 p-16 z-[0]"
+      className="flex flex-col justify-center items-center space-y-8 md:space-x-14 min-h-[25rem] bg-gradient-to-br from-indigo-600 via-purple-600 to-fuchsia-500 px-8 py-16 md:p-16 z-[0]"
     >
       <div className="space-y-8">
         <div className="max-w-5xl space-y-8 text-slate-50">
           <h1 className=" font-bold text-3xl text-slate-100">About Me.</h1>
           <div className="text-lg space-y-6 font-light">
             <p>
-              Hey, my name is Maaz Siddiqi! I'm a passionate self-taught
-              developer who just started studying at Western University in Fall
-              2021. I'm enrolled in a Business and Computer Science double
-              degree program, and its been a blast so far!
+              I'm a passionate self-taught developer who just started studying
+              at Western University in Fall 2021. I'm enrolled in a Business and
+              Computer Science double degree program, and its been a blast so
+              far!
             </p>
             <p>
               Fun fact, I've actually moved around 10 times in my life already!
