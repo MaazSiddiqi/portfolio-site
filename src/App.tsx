f6a18d686c994dfd7ea0ba3071aae72b4fd34ca2
Maaz Ali Siddiqi <msidd54@uwo.ca>
2025-05-28 22:39:49 -0400

    init hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex flex-col justify-center h-screen p-72">
+      <div className="absolute top-0 left-0 w-full h-full bg-[url('/images/name-lg.png')] bg-cover bg-center"></div>
+      <div className="flex flex-col gap-4">
+        <div className="text-3xl leading-title">
+          <h1>
+            Hey, I&apos;m <span className="text-light">Maaz.</span>
+          </h1>
+          <h1>
+            I&apos;m a <span className="text-light">software engineer</span>{" "}
+            building systems <br /> for the future.
+          </h1>
+        </div>
+        <div>
+          Currently a software engineering intern at{" "}
+          <span className="text-light">Amazon Web Services</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
