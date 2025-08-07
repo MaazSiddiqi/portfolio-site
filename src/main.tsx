494a697852864b0e5eeec0f83e2d85c859e0a4ee
Maaz Ali Siddiqi <msidd54@uwo.ca>
2025-08-07 00:08:11 -0700

    upgrade packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { CursorProvider } from "./contexts/CursorProvider.tsx";
-import { Analytics } from "@vercel/analytics/next";
-import "./index.css";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.tsx"
+import { CursorProvider } from "./contexts/CursorProvider.tsx"
+import { Analytics } from "@vercel/analytics/react"
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,4 +12,4 @@ createRoot(document.getElementById("root")!).render(
       <Analytics />
     </CursorProvider>
   </StrictMode>,
-);
+)
