08e388a8f3068fcf46830fae5f90a719d7fe971e
Maaz Ali Siddiqi <msidd54@uwo.ca>
2025-08-07 00:09:21 -0700

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import { CursorProvider } from "./contexts/CursorProvider.tsx"
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
